175da941e94a0ea493774dc981b5a655fc93e01d
gaticks <kenneth.norholm@gmail.com>
2019-08-22 15:13:40 +0200

    created the initial c# wrapper for the mouse control API CC-50

---
+++ src/MouseControlApiTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using mouse_control_api;
+using System;
+/// <summary>
+/// is intended for manual testing and therefore is not names "Test" with a lower case "t".
+/// </summary>
+namespace MouseControlApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mouseControlApi = new MouseControlApi();
+            mouseControlApi.moveMouse(200, 200);
+            mouseControlApi.moveMouse(400, 200);
+            mouseControlApi.moveMouse(200, 400);
+            mouseControlApi.moveMouse(800, 200);
+            mouseControlApi.moveMouse(200, 800);
+            mouseControlApi.moveMouse(1600, 600);
+
+            Console.ReadKey();
+        }
+    }
+}
